53d9779bc71588d596c5afa5f6c6f21bec65c1f1
Harold Martin <harold.martin@gmail.com>
2024-07-11 12:07:30 -0700

    Auto request missing bodies, add browser column, factor out enums

--- safari_to_sqlite/constants.py
+++ safari_to_sqlite/constants.py
@@ -1,8 +1,11 @@
+from enum import Enum
+
 BODY = "body"
+BROWSER = "browser"
 FIRST_SEEN = "firstSeen"
 HOST = "host"  # refers to user's local hostname NOT the URL host
 SCRAPE_STATUS = "scrapeStatus"
-TABS = "safari_tabs"
+TABS = "tabs"
 TAB_INDEX = "tabIndex"
 TITLE = "title"
 URL = "url"
@@ -12,8 +15,20 @@ TURSO_SAFARI = "turso_safari"
 TURSO_URL = "turso_url"
 TURSO_AUTH_TOKEN = "turso_auth_token"  # noqa: S105
 
-NOT_SCRAPED = -1
-EXTRACT_FAILED = -2
-UNICODE_FAILED = -3
+
+class ScrapeStatus(Enum):
+    """Enum for scrape status."""
+
+    NotScraped = -1
+    ExtractFailed = -2
+    UnicodeFailed = -3
+
+
+class Browser(Enum):
+    """Enum for browser."""
+
+    Safari = "safari"
+    Chrome = "chrome"
+
 
 SEP = "150r2M72e7D6Lb7Z9u1g4BSQBonv6U21W1fmX8B1TXR8XXqB2wTgQzqwoB06144d"
